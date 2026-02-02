97d4e9891e69f7a2fff955b2f032c3b8c70e7e4a
turbomaxee <137450788+TurboMaxe@users.noreply.github.com>
2026-02-03 00:28:48 +0500

    fix

--- cards/__init__.py
+++
@@ -1,3 +0,0 @@
-from .resources import load_all, get_font, get_template
-from .lifestats import generate_lifestats_card, generate_lifestats_card_async
-from .duelstats import generate_duelstats_card, generate_duelstats_card_async
